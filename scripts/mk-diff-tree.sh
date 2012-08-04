da2d7bde0ee240c21482fcc88342e1ddcda5b443
Krystian Bacławski <krystian.baclawski@gmail.com>
2012-08-04 13:01:59 +0200

    Update patches.

---
+++ scripts/mk-diff-tree.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export LC_ALL=C
+
+diff=gdiff
+
+$diff -durq "$1.orig" "$1" | while read -a line; do
+  case "${line[0]}" in
+    "Files")
+      orig="${line[1]}"
+      file="${line[3]}"
+      dir="../patches/$(dirname $file)"
+      patch="${dir}/$(basename ${file}).diff"
+      echo "${orig} vs. ${file} -> ${patch}"
+      mkdir -p "${dir}"
+      $diff -du "${orig}" "${file}" | sed -e "s/$1.orig/$1/" >"${patch}"
+      ;;
+    "Only")
+      file="${line[2]%:}/${line[3]}"
+      dir="../patches/${line[2]%:}"
+      mkdir -p "${dir}"
+      cp -v "${file}" "${dir}/"
+      ;;
+    *)
+      ;;
+  esac
+done
